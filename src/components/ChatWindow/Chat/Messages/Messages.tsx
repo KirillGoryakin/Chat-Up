f5f9d6e5210bfa3d04b83135d22c59159b2b04ae
KirillGoryakin <kirillgoryakin.dev@gmail.com>
2023-01-16 22:17:19 +0500

    Add auto scroll

--- src/components/ChatWindow/Chat/Messages/Messages.tsx
+++ src/components/ChatWindow/Chat/Messages/Messages.tsx
@@ -1,5 +1,5 @@
 import { Flex } from '@chakra-ui/react';
-import { useEffect } from 'react';
+import { createRef, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from 'hooks/reduxHooks';
 import { Message } from './Message';
 import { doc, onSnapshot } from 'firebase/firestore';
@@ -9,6 +9,8 @@ import { Message as MessageType } from 'appTypes';
 import { AnimatePresence } from 'framer-motion';
 
 const Messages = () => {
+  const ref = createRef<HTMLDivElement>();
+  
   const dispatch = useAppDispatch();
   const state = useAppSelector(state => state.auth);
   const user = state.user;
@@ -35,15 +37,20 @@ const Messages = () => {
       unsub();
     };
   }, [chatId, dispatch]);
+
+  useEffect(() => {
+    if (ref.current)
+      ref.current.scrollTop = ref.current.scrollHeight;
+  }, [messages, ref]);
   
   return (
     <Flex
+      ref={ref}
       w='100%'
       overflowX='hidden'
       overflowY='auto'
       flexDirection='column'
-      px={5}
-      pt={2}
+      px={5} pt={2}
     >
       <AnimatePresence>
         {user && messages &&
